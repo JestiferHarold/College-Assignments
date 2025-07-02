98faab2ec2dcac1311b1202e7d4fa98006ccdb99
JestiferHarold <Sanjay.Pandian@tutamail.com>
2025-07-02 22:09:12 +0530

    doc: added pynb and pdfs from ai

---
+++ Sem 3/C/CSP.c
@@ -0,0 +1,14 @@
+/**
+ * author: P Sanjay
+ * date:
+ * subject: C programming
+ * topic: control structure practice
+ */
+
+#include <stdio.h>
+
+void question1();
+void question2();
+void question3();
+void question4();
+void 
